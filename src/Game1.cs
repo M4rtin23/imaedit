1ab3510db25ad60e945169f2bac18cd65333df2b
martin <martunico23@hotmail.com>
2024-12-19 22:59:18 -0300

    Improve performance
    By updating only the contents of colorList that are modified

--- src/Game1.cs
+++ src/Game1.cs
@@ -4,14 +4,13 @@ using Microsoft.Xna.Framework.Input;
 using System.Drawing;
 
 
-
 namespace imaedit;
 
 public class Game1 : Game{
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 	private float scaling = 1;
-	public static int height = 50, width = 80, size = 5;
+	public static int height = 50*5, width = 80*5, size = 1;
 
 	public static float rotationX = 0;
 	public static float rotationY = 0;
@@ -28,6 +27,7 @@ public class Game1 : Game{
 	private static GameBuilder.Shapes.RectangleF[,] pixels = new GameBuilder.Shapes.RectangleF[width,height];
 	private Microsoft.Xna.Framework.Color selectedColor = Microsoft.Xna.Framework.Color.Pink;
 
+	public Microsoft.Xna.Framework.Color[] colorList = new Microsoft.Xna.Framework.Color[height*width];
 
     public Game1(){
         _graphics = new GraphicsDeviceManager(this);
@@ -42,6 +42,12 @@ public class Game1 : Game{
 			for(int j = 0; j < height; j++){
 				pixels[i,j] = new GameBuilder.Shapes.RectangleF(size*i,size*j,size,size);
 			}
+		}
+		for(int i = 0; i < height; i++){
+			for(int j = 0; j < width; j++){
+				colorList[i*width+j] = pixels[j,i].GetColor();
+			}
+
 		}
 
         base.Initialize();
@@ -58,12 +64,19 @@ public class Game1 : Game{
 					for(int j = 0; j < height; j++){
 						if(pixels[i,j].Contains(Mouse.GetState().Position.ToVector2()/scaling-position/scaling)){
 							pixels[i,j].SetColor(selectedColor);
+
 						}
 					}
 				}
 				*/
+
 				try{
-					pixels[(int)((Mouse.GetState().Position.X-position.X)/size/scaling),(int)((Mouse.GetState().Position.Y-position.Y)/size/scaling)].SetColor(selectedColor);
+					int i = (int)((Mouse.GetState().Position.X-position.X)/size/scaling);
+					int j = (int)((Mouse.GetState().Position.Y-position.Y)/size/scaling);
+					pixels[i,j].SetColor(selectedColor);
+
+					colorList[j*width+i] = selectedColor;//pixels[j,i].GetColor();
+
 				}catch{}
 
 
@@ -160,45 +173,19 @@ public class Game1 : Game{
 
 
 		Texture2D texture = new Texture2D(GraphicsDevice, width, height);
-
-		Microsoft.Xna.Framework.Color[] colorList = new Microsoft.Xna.Framework.Color[height*width];
-
-		for(int i = 0; i < height; i++){
-			for(int j = 0; j < width; j++){
-				colorList[i*width+j] = pixels[j,i].GetColor();
-			}
-
-		}
-
-		
 		texture.SetData(colorList);
 
-/*		_spriteBatch.Begin(transformMatrix: Matrix.CreateScale(scaling)*Matrix.CreateRotationZ(rotationZ)*Matrix.CreateRotationY(rotationY)*Matrix.CreateRotationX(rotationX)*Matrix.CreateTranslation(position.X, position.Y, 0), samplerState:  SamplerState.PointClamp);
-
-
-		for(int i = 0; i < width; i++){
-			for(int j = 0; j < height; j++){
-				pixels[i,j].Draw(_spriteBatch);
-			}
-		}
-		_spriteBatch.End();*/
 		_spriteBatch.Begin(transformMatrix: Matrix.CreateScale(scaling*size)*Matrix.CreateRotationZ(rotationZ)*Matrix.CreateRotationY(rotationY)*Matrix.CreateRotationX(rotationX)*Matrix.CreateTranslation(position.X, position.Y, 0), samplerState:  SamplerState.PointClamp);
-
 		_spriteBatch.Draw(texture, Vector2.Zero, Microsoft.Xna.Framework.Color.White);
-
-//		new GameBuilder.Shapes.RectangleF(Mouse.GetState().Position.ToVector2(), new Vector2(20,20)).Draw(_spriteBatch);
 		_spriteBatch.End();
 
 		_spriteBatch.Begin();
-
-
 	/*	int size = 30;
 		new GameBuilder.Shapes.RectangleF(0, _graphics.PreferredBackBufferHeight/2-size*2, size, size, Microsoft.Xna.Framework.Color.Red).Draw(_spriteBatch);
 		new GameBuilder.Shapes.RectangleF(size, _graphics.PreferredBackBufferHeight/2-size*2, size, size, Microsoft.Xna.Framework.Color.Green).Draw(_spriteBatch);
 		new GameBuilder.Shapes.RectangleF(0, _graphics.PreferredBackBufferHeight/2-size, size, size, Microsoft.Xna.Framework.Color.Blue).Draw(_spriteBatch);
 		new GameBuilder.Shapes.RectangleF(size, _graphics.PreferredBackBufferHeight/2-size, size, size, Microsoft.Xna.Framework.Color.Pink).Draw(_spriteBatch);
 */
-
 		_spriteBatch.End();
         base.Draw(gameTime);
     }
